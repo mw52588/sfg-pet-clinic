f48a1c3ff05c7edd65f72a0edf49250ffda1e40f
Matthew <mw52588@gmail.com>
2019-11-03 15:48:45 -0800

    Added maven build plugin

---
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTest.java
@@ -0,0 +1,17 @@
+package guru.springframework.sfgpetclinic;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.*;
+
+@SpringBootTest
+public class SfgPetClinicApplicationTest {
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
