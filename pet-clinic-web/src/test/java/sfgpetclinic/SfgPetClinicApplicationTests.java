670b6e4c392732c5da9c99e70e38a6c6e1d38b75
Matthew <mw52588@gmail.com>
2019-11-03 14:55:32 -0800

    refactoring to multimodule maven projects. closes #1.

--- pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,6 +1,7 @@
-package guru.springframework.sfgpetclinic;
+package sfgpetclinic;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -12,5 +13,4 @@ class SfgPetClinicApplicationTests {
     @Test
     void contextLoads() {
     }
-
 }
