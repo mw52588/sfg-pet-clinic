1577ebf7d7b77dcce0ec706584600f76c741d53b
Matthew <mw52588@gmail.com>
2019-11-03 19:01:20 -0800

    Added Index.html and IndexController. closes #8.

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index() {
+        return "index";
+    }
+}
